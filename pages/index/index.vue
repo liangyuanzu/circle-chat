34c900d267868f13e508800b7d2b4d2648480d32
liangyuanzu <liangyuanzu@gmail.com>
2020-08-06 23:40:21 +0800

    feat(app): init

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
