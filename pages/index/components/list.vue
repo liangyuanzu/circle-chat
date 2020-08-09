9db32dd0a269070bd203dfef0e6299f5bd5ff0bb
liangyuanzu <liangyuanzu@gmail.com>
2020-08-09 17:49:27 +0800

    feat(list): 附近的圈列表

---
+++ pages/index/components/list.vue
@@ -0,0 +1,62 @@
+<template>
+	<view>
+		<uni-list>
+		    <uni-list :border="true">
+		        <uni-list-chat v-for="(item, index) in list" :key="index" :title="item.title" :avatar="item.img_url" :note="item.note">
+		            <view class="chat-custom-right">
+		                <text class="chat-custom-text">
+											<text>人数 {{item.people_num}}</text>
+											<text>范围：{{item.range}}</text>
+										</text>
+		            </view>
+		        </uni-list-chat>
+		    </uni-list>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		components: {},
+		data() {
+			return {
+				list: [{
+					title: '圈名称',
+					img_url: 'https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png',
+					note: '您收到一条新的消息',
+					people_num: '999',
+					range: '500m'
+				},{
+					title: '圈名称',
+					img_url: 'https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png',
+					note: '您收到一条新的消息',
+					people_num: '999',
+					range: '500m'
+				},{
+					title: '圈名称',
+					img_url: 'https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png',
+					note: '您收到一条新的消息',
+					people_num: '999',
+					range: '500m'
+				}]
+			}
+		},
+
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.chat-custom-right {
+	.chat-custom-text {
+		font-size: 12px;
+		color: #999;
+
+		text:nth-of-type(1) {
+			margin-right: 50rpx;
+		}
+	}
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,17 +2,20 @@
 	<view>
 		<nav-bar />
 		<tabs />
+		<list />
 	</view>
 </template>
 
 <script>
 	import navBar from './components/nav-bar.vue'
 	import tabs from './components/tabs.vue'
+	import list from './components/list.vue'
 
 	export default {
 		components: {
 			'nav-bar': navBar,
-			tabs
+			tabs,
+			list
 		},
 		data() {
 			return {
