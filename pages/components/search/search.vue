354f322e8d185751b5bf1e91c6bdcbed3c5f0399
liangyuanzu <liangyuanzu@gmail.com>
2020-08-13 23:40:30 +0800

    feat(search): 搜索页面

---
+++ pages/components/search/search.vue
@@ -0,0 +1,35 @@
+<template>
+	<view :style="style">
+		<search />
+	</view>
+</template>
+
+<script>
+	import search from '@/components/HM-search/HM-search.vue'
+
+	export default {
+		components: {
+			search
+		},
+
+		name: 'cu-custom',
+
+		data() {
+			return {
+
+			};
+		},
+
+		computed: {
+			style() {
+				var StatusBar= this.StatusBar;
+				var style = `margin-top:${StatusBar}px;`;
+				return style
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
