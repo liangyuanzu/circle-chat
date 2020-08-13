0f5b71af7d4526fd7289f0682e726847ca0fb3c8
liangyuanzu <liangyuanzu@gmail.com>
2020-08-13 22:18:24 +0800

    feat(list): 聊天列表

--- pages/message/message.vue
+++ pages/message/message.vue
@@ -1,16 +1,18 @@
 <template>
 	<view>
-		<!-- 导航栏 -->
 		<nav-bar />
+		<list />
 	</view>
 </template>
 
 <script>
 	import navBar from './components/nav-bar.vue'
+	import list from './components/list.vue'
 
 	export default {
 		components: {
-			'nav-bar': navBar
+			'nav-bar': navBar,
+			list
 		},
 		data() {
 			return {
