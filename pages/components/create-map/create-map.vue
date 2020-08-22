418496a88cf21f4d41ef4f66e92db9c09594dcdb
liangyuanzu <liangyuanzu@gmail.com>
2020-08-23 00:54:13 +0800

    feat(create): 地图创建模式结构完成

---
+++ pages/components/create-map/create-map.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<web-view src="/hybrid/html/create-map.html"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
