850a113494a6ecb9b2daa661499f4403280054e8
liangyuanzu <liangyuanzu@gmail.com>
2020-09-09 10:08:22 +0800

    feat(reg): 注册

--- api/user.js
+++ api/user.js
@@ -11,3 +11,30 @@ export function login(data) {
 		data
 	})
 }
+
+/**
+ * 发送邮箱验证码(注册)
+ * @param {string} email - 邮箱号
+ * */
+export function sendRegisterEmail(email) {
+	return $request('/user/sendRegisterEmail', {
+		params: {
+			email
+		}
+	})
+}
+
+/**
+ * 注册
+ * @param {string} username - 用户名
+ * @param {string} password - 密码
+ * @param {string} rePassword - 确认密码
+ * @param {string} email - 邮箱号
+ * @param {string} code - 邮箱验证码
+ * */
+export function register(data) {
+	return $request('/user/register', {
+		method: 'POST',
+		data
+	})
+}
--- store/modules/user.js
+++ store/modules/user.js
@@ -1,5 +1,12 @@
-import { login } from '@/api/user.js'
-import { setToken, removeToken } from "@/helpers/token.js";
+import {
+	login,
+	sendRegisterEmail,
+	register
+} from '@/api/user.js'
+import {
+	setToken,
+	removeToken
+} from "@/helpers/token.js";
 
 const state = {
 	username: ''
@@ -11,22 +18,45 @@ const getters = {
 
 const mutations = {
 	setUsername(state, username) {
-    state.username = username;
-  },
+		state.username = username;
+	},
 };
 
 const actions = {
-	async login({ commit }, userInfo) {
-	  const { token, username } = await login(userInfo);
+	async login({
+		commit
+	}, userInfo) {
+		const {
+			token,
+			username
+		} = await login(userInfo);
 		if (token && username) {
 			setToken(token)
 			commit("setUsername", username);
-		}else {
+		} else {
 			uni.showToast({
 				icon: 'none',
 				title: '登录接口异常'
 			});
 		}
 	},
+
+	async sendRegisterEmail({
+		commit
+	}, email) {
+		await sendRegisterEmail(email)
+	},
+
+	async register({
+		commit
+	}, regInfo) {
+		await register(regInfo);
+	},
 };
-export default { state, getters, mutations, actions };
+
+export default {
+	state,
+	getters,
+	mutations,
+	actions
+};
