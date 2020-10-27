a95c8df4bcf4b99d59444be6f37511eca03b9ae2
liangyuanzu <liangyuanzu@gmail.com>
2020-10-27 21:51:54 +0800

    feat(me): operateList 操作列表组件封装

---
+++ pages/me/components/operateList.vue
@@ -0,0 +1,216 @@
+<template>
+  <view style="margin-top: 20rpx">
+    <uni-list>
+      <!-- 个人信息 关注列表 -->
+      <uni-list-item
+        v-for="(item, index) in list"
+        :key="index"
+        :title="item.title"
+        show-extra-icon
+        :extraIcon="item.extraIcon"
+        showArrow
+        clickable
+        @click="listItemClick(item.title)"
+      >
+      </uni-list-item>
+
+      <!-- 默认加圈 -->
+      <uni-list-item
+        :title="defalutAdd.title"
+        show-extra-icon
+        :extraIcon="defalutAdd.extraIcon"
+      >
+        <template #right>
+          <u-switch
+            size="40"
+            v-model="addChecked"
+            :loading="addLoading"
+          ></u-switch>
+        </template>
+      </uni-list-item>
+
+      <!-- 消息通知 -->
+      <uni-list-item
+        :title="notice.title"
+        show-extra-icon
+        :extraIcon="notice.extraIcon"
+      >
+        <template #right>
+          <u-switch
+            size="40"
+            v-model="noticeChecked"
+            :loading="noticeLoading"
+          ></u-switch>
+        </template>
+      </uni-list-item>
+    </uni-list>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      addChecked: false,
+      noticeChecked: false,
+      addLoading: false,
+      noticeLoading: false,
+      // 中间变量，避免在watch中多次回调，造成无限循环
+      addOpenStatus: false,
+      addCloseStatus: false,
+      noticeOpenStatus: false,
+      noticeCloseStatus: false,
+
+      list: [
+        {
+          title: '个人信息',
+          extraIcon: {
+            color: '#2979ff',
+            size: '22',
+            type: 'contact-filled'
+          }
+        },
+        {
+          title: '关注列表',
+          extraIcon: {
+            color: '#ff9900',
+            size: '22',
+            type: 'star-filled'
+          }
+        }
+      ],
+
+      defalutAdd: {
+        title: '默认加圈',
+        extraIcon: {
+          color: '#909399',
+          size: '22',
+          type: 'circle-filled'
+        }
+      },
+
+      notice: {
+        title: '消息通知',
+        extraIcon: {
+          color: '#ff9900',
+          size: '22',
+          type: 'sound-filled'
+        }
+      }
+    }
+  },
+
+  watch: {
+    addChecked(val) {
+      if (val) {
+        if (this.addOpenStatus) {
+          this.addOpenStatus = false
+          return
+        }
+        // 避免造成无限循环
+        this.addCloseStatus = true
+        this.addChecked = false
+        this.addLoading = true
+        this.$store
+          .dispatch('user/userSet', {
+            defaultAdd: '111'
+          })
+          .then(() => {
+            this.addOpenStatus = true
+            this.addChecked = true
+            this.addLoading = false
+          })
+          .catch(() => (this.addLoading = false))
+      } else {
+        if (this.addCloseStatus) {
+          this.addCloseStatus = false
+          return
+        }
+        this.addOpenStatus = true
+        this.addChecked = true
+        this.addLoading = true
+        this.$store
+          .dispatch('user/userSet', {
+            defaultAdd: '000'
+          })
+          .then(() => {
+            this.addCloseStatus = true
+            this.addChecked = false
+            this.addLoading = false
+          })
+          .catch(() => (this.addLoading = false))
+      }
+    },
+
+    noticeChecked(val) {
+      if (val) {
+        if (this.noticeOpenStatus) {
+          this.noticeOpenStatus = false
+          return
+        }
+        // 避免造成无限循环
+        this.noticeCloseStatus = true
+        this.noticeChecked = false
+        this.noticeLoading = true
+        this.$store
+          .dispatch('user/userSet', {
+            isNotice: 0
+          })
+          .then(() => {
+            this.noticeOpenStatus = true
+            this.noticeChecked = true
+            this.noticeLoading = false
+          })
+          .catch(() => (this.noticeLoading = false))
+      } else {
+        if (this.noticeCloseStatus) {
+          this.noticeCloseStatus = false
+          return
+        }
+        this.noticeOpenStatus = true
+        this.noticeChecked = true
+        this.noticeLoading = true
+        this.$store
+          .dispatch('user/userSet', {
+            defaultAdd: '000',
+            isNotice: 1
+          })
+          .then(() => {
+            this.noticeCloseStatus = true
+            this.noticeChecked = false
+            this.noticeLoading = false
+          })
+          .catch(() => (this.noticeLoading = false))
+      }
+    }
+  },
+
+  methods: {
+    listItemClick(title) {
+      switch (title) {
+        case '个人信息':
+          this.toUserinfo()
+          break
+        case '关注列表':
+          this.toFocusList()
+          break
+        default:
+          break
+      }
+    },
+
+    toUserinfo() {
+      this.$emit('toInfo')
+    },
+
+    toFocusList() {
+      uni.navigateTo({
+        url: '/pages/components/focus-list/focus-list'
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
