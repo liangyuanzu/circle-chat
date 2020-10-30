80a4c5c12aa6a4432dd0507f83d0c2debe962f75
liangyuanzu <liangyuanzu@gmail.com>
2020-10-31 00:10:04 +0800

    feat(circle): 圈子页样式调整

--- pages/circle/circle.vue
+++ pages/circle/circle.vue
@@ -1,29 +1,62 @@
 <template>
   <view>
-    <nav-bar />
+    <view class="bar">
+      <u-search
+        placeholder="搜索圈"
+        v-model="keyword"
+        :clearabled="false"
+        :show-action="false"
+        class="search"
+      ></u-search>
+      <view class="create">
+        <text>创建圈</text>
+        <uni-icons size="16" color="#c0c4cc" type="plusempty"> </uni-icons>
+      </view>
+    </view>
     <tabs />
     <circle-list />
   </view>
 </template>
 
 <script>
-import navBar from './components/nav-bar.vue'
 import tabs from './components/tabs.vue'
 import circleList from './components/circle-list.vue'
 
 export default {
   components: {
-    'nav-bar': navBar,
     tabs,
     'circle-list': circleList
   },
   data() {
-    return {}
+    return {
+      keyword: ''
+    }
   },
   onLoad() {},
   methods: {}
 }
 </script>
 
-<style>
+<style lang="scss">
+.bar {
+  width: 100%;
+  height: 80rpx;
+  line-height: 80rpx;
+  position: relative;
+  background-color: #fff;
+
+  .search {
+    width: 60%;
+    position: relative;
+    left: 10%;
+  }
+
+  .create {
+    position: absolute;
+    right: 10%;
+    top: 50%;
+    transform: translateY(-50%);
+    color: rgb(96, 98, 102);
+  }
+}
 </style>
