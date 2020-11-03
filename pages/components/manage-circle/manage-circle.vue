e1c6521730e1a8a3db799fb409da36df2536b23e
liangyuanzu <liangyuanzu@gmail.com>
2020-11-03 20:05:11 +0800

    feat(manage-circle): 管理圈

---
+++ pages/components/manage-circle/manage-circle.vue
@@ -0,0 +1,39 @@
+<template>
+  <view>
+    <uni-list>
+      <uni-list-item
+        title="编辑资料"
+        :border="false"
+        showArrow
+        clickable
+        @click="toEditCircleInfo"
+      >
+      </uni-list-item>
+    </uni-list>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      circleId: ''
+    }
+  },
+
+  onLoad({ circleId }) {
+    this.circleId = circleId
+  },
+
+  methods: {
+    toEditCircleInfo() {
+      this.$u.route('/pages/components/edit-circle-info/edit-circle-info', {
+        circleId: this.circleId
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
