e39cb27dfadb748fedba778ea09a1006fe4b245c
liangyuanzu <liangyuanzu@gmail.com>
2020-11-09 17:24:53 +0800

    feat(config): websocket

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	baseUrl: 'https://www.circlechat.top',
 	accessTokenName: 'accessToken',
 	refreshTokenName: 'refreshToken',
-	websocketUrl: 'ws://39.99.156.255:8081/websocket',
+	websocketUrl: 'wss://www.circlechat.top/websocket',
 	tabBarIndex: 0, // 消息提示tabBar索引
 	sendOneType: 'SEND_TO_ONE_REQUEST',
 	receiveOneType: 'SEND_TO_USER_REQUEST',
