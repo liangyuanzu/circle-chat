347d187a8ec4246bcea97e0c442c7cf0650d814d
liangyuanzu <liangyuanzu@gmail.com>
2020-11-15 14:26:16 +0800

    feat(nearby-circle-map): url

--- config/config.js
+++ config/config.js
@@ -3,6 +3,8 @@ module.exports = {
 	websocketUrl: 'wss://www.circlechat.top/websocket',
 	createCircleMapUrl:
 		'https://www.circlechat.top/static/hybrid/html/create-circle-map.html',
+	nearbyCircleMapUrl:
+		'https://www.circlechat.top/static/hybrid/html/nearby-circle-map.html',
 	tabBarIndex: 0, // 消息提示tabBar索引
 	sendOneType: 'SEND_TO_ONE_REQUEST',
 	receiveOneType: 'SEND_TO_USER_REQUEST',
--- pages/components/nearby-circle-map/nearby-circle-map.vue
+++ pages/components/nearby-circle-map/nearby-circle-map.vue
@@ -1,13 +1,18 @@
 <template>
   <view>
-    <web-view src="/hybrid/html/nearby-circle-map.html"></web-view>
+    <!-- <web-view src="/hybrid/html/nearby-circle-map.html"></web-view> -->
+    <web-view :src="nearbyCircleMapUrl"></web-view>
   </view>
 </template>
 
 <script>
+import { nearbyCircleMapUrl } from '@/config/config.js'
+
 export default {
   data() {
-    return {}
+    return {
+      nearbyCircleMapUrl
+    }
   }
 }
 </script>
