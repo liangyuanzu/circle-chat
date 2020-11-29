dc9748a5a358049cde3269c6603091775aa35ed6
liangyuanzu <liangyuanzu@gmail.com>
2020-11-29 08:52:46 +0800

    feat: 封装自定义圈详情组件

---
+++ components/custom-circle-detail/custom-circle-detail.vue
@@ -0,0 +1,112 @@
+<template>
+  <view>
+    <custom-avatar
+      :src="circleInfo.img"
+      :title="circleInfo.circleName"
+      :note="circleInfo.circleId"
+    >
+    </custom-avatar>
+
+    <view class="text-df text-grey padding bg-white margin-top-sm">
+      <view>本圈创建于 {{ createTime }}</view>
+      <view v-if="circleInfo.synopsis">{{ circleInfo.synopsis }}</view>
+    </view>
+
+    <view
+      class="text-df text-grey padding bg-white margin-top-sm"
+      v-if="circleInfo.explain"
+    >
+      <view>入圈说明</view>
+      <view>{{ circleInfo.explain }}</view>
+    </view>
+
+    <view class="margin-top-sm">
+      <uni-list>
+        <uni-list-item title="圈类型">
+          <template #footer>
+            <u-tag :text="circleInfo.type" size="mini" :type="formatType" />
+          </template>
+        </uni-list-item>
+        <uni-list-item title="圈人数">
+          <template #footer>
+            <view class="cu-capsule radius flex">
+              <view class="cu-tag bg-blue sm">
+                <text class="cuIcon-group"></text>
+              </view>
+              <view class="cu-tag line-blue sm">
+                {{ circleInfo.member }}
+              </view>
+            </view>
+          </template>
+        </uni-list-item>
+        <uni-list-item title="圈范围">
+          <template #footer>
+            <view class="cu-capsule radius">
+              <view class="cu-tag bg-black sm">
+                <text class="cuIcon-radiobox"></text>
+              </view>
+              <view class="cu-tag line-black sm">
+                {{ $formatRadius(circleInfo.radius) }}</view
+              >
+            </view>
+          </template>
+        </uni-list-item>
+      </uni-list>
+    </view>
+
+    <view class="margin-top-sm">
+      <uni-list>
+        <uni-list-item title="圈主" showArrow clickable @click="toOwner">
+          <template #footer>
+            <u-avatar
+              :src="circleInfo.userImg"
+              mode="square"
+              size="mini"
+            ></u-avatar>
+          </template>
+        </uni-list-item>
+      </uni-list>
+    </view>
+  </view>
+</template>
+
+<script>
+/**
+ * custom-circle-detail 自定义圈列表组件
+ * @description 本组件是适用于本项目的圈详情组件，一般用于展示圈详情的地方。
+ * @property {Object} 	circleInfo 	圈数据
+ */
+export default {
+  name: 'custom-circle-detail',
+
+  props: {
+    circleInfo: {
+      type: Object,
+      default: {}
+    }
+  },
+
+  computed: {
+    createTime() {
+      return /(\d{4})\-(\d{2})\-(\d{2})/
+        .exec(this.circleInfo.createTime)[0]
+        .replace(/(\d{4})\-(\d{2})\-(\d{2})/, '$1年$2月$3日')
+    },
+    formatType() {
+      if (this.circleInfo.type === '固定圈') return 'warning'
+      if (this.circleInfo.type === '紧急圈') return 'error'
+    }
+  },
+
+  methods: {
+    toOwner() {
+      this.$u.route('/pages/components/person-info/person-info', {
+        id: this.circleInfo.userId
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- pages/components/receive-invite-list/to-circle-invite/to-circle-invite.vue
+++ pages/components/receive-invite-list/to-circle-invite/to-circle-invite.vue
@@ -1,72 +1,6 @@
 <template>
   <view>
-    <custom-avatar
-      :src="circleInfo.img"
-      :title="circleInfo.circleName"
-      :note="circleInfo.circleId"
-    >
-    </custom-avatar>
-
-    <view class="text-df text-grey padding bg-white margin-top-sm">
-      <view>本圈创建于 {{ createTime }}</view>
-      <view v-if="circleInfo.synopsis">{{ circleInfo.synopsis }}</view>
-    </view>
-
-    <view
-      class="text-df text-grey padding bg-white margin-top-sm"
-      v-if="circleInfo.explain"
-    >
-      <view>入圈说明</view>
-      <view>{{ circleInfo.explain }}</view>
-    </view>
-
-    <view class="margin-top-sm">
-      <uni-list>
-        <uni-list-item title="圈类型">
-          <template #footer>
-            <u-tag :text="circleInfo.type" size="mini" :type="formatType" />
-          </template>
-        </uni-list-item>
-        <uni-list-item title="圈人数">
-          <template #footer>
-            <view class="cu-capsule radius flex">
-              <view class="cu-tag bg-blue sm">
-                <text class="cuIcon-group"></text>
-              </view>
-              <view class="cu-tag line-blue sm">
-                {{ circleInfo.member }}
-              </view>
-            </view>
-          </template>
-        </uni-list-item>
-        <uni-list-item title="圈范围">
-          <template #footer>
-            <view class="cu-capsule radius">
-              <view class="cu-tag bg-black sm">
-                <text class="cuIcon-radiobox"></text>
-              </view>
-              <view class="cu-tag line-black sm">
-                {{ $formatRadius(circleInfo.radius) }}</view
-              >
-            </view>
-          </template>
-        </uni-list-item>
-      </uni-list>
-    </view>
-
-    <view class="margin-top-sm">
-      <uni-list>
-        <uni-list-item title="圈主" showArrow clickable @click="toOwner">
-          <template #footer>
-            <u-avatar
-              :src="circleInfo.userImg"
-              mode="square"
-              size="mini"
-            ></u-avatar>
-          </template>
-        </uni-list-item>
-      </uni-list>
-    </view>
+    <custom-circle-detail :circleInfo="circleInfo"></custom-circle-detail>
 
     <view class="padding margin-top-lg" v-if="handle">
       <u-button style="width: 100%">{{ agree ? '已同意' : '已拒绝' }}</u-button>
@@ -97,16 +31,7 @@ export default {
   },
 
   computed: {
-    ...mapGetters('circle', ['circleInfo']),
-    createTime() {
-      return /(\d{4})\-(\d{2})\-(\d{2})/
-        .exec(this.circleInfo.createTime)[0]
-        .replace(/(\d{4})\-(\d{2})\-(\d{2})/, '$1年$2月$3日')
-    },
-    formatType() {
-      if (this.circleInfo.type === '固定圈') return 'warning'
-      if (this.circleInfo.type === '紧急圈') return 'error'
-    }
+    ...mapGetters('circle', ['circleInfo'])
   },
 
   onLoad({ circleId, handle, agree }) {
@@ -116,12 +41,6 @@ export default {
   },
 
   methods: {
-    toOwner() {
-      this.$u.route('/pages/components/person-info/person-info', {
-        id: this.circleInfo.userId
-      })
-    },
-
     inviteHandle(flag) {
       uni.showLoading({
         title: '加载中...'
