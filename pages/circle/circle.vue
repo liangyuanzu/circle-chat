59a27afffd1d644e66650e9a51b3d801bd4485ab
liangyuanzu <liangyuanzu@gmail.com>
2020-12-05 16:35:55 +0800

    feat(modal): 增加创建圈弹窗入口

--- pages/circle/circle.vue
+++ pages/circle/circle.vue
@@ -11,9 +11,35 @@
         ></u-search>
       </view>
 
-      <view class="create" @click="createCircle">
+      <view class="create" @tap="showModal" data-target="RadioModal">
         <text>创建圈</text>
         <uni-icons size="16" color="#c0c4cc" type="plusempty"> </uni-icons>
+      </view>
+
+      <view
+        class="cu-modal"
+        :class="modalName == 'RadioModal' ? 'show' : ''"
+        @tap="hideModal"
+      >
+        <text class="lg text-white cuIcon-usefullfill custom-icon"></text>
+        <view class="cu-dialog cutom-modal" @tap.stop="">
+          <view class="cu-list menu sm-border">
+            <view
+              class="cu-item"
+              v-for="(item, index) in list"
+              :key="index"
+              @tap="itemClick(item.text)"
+            >
+              <view class="content">
+                <text
+                  class="lg text-grey"
+                  :class="'cuIcon-' + item.icon"
+                ></text>
+                <text class="text-grey">{{ item.text }}</text>
+              </view>
+            </view>
+          </view>
+        </view>
       </view>
     </view>
 
@@ -31,24 +57,44 @@ export default {
 
   data() {
     return {
-      keyword: ''
+      modalName: null,
+      radio: 'radio1',
+      list: [
+        {
+          icon: 'locationfill',
+          text: '地图模式'
+        },
+        {
+          icon: 'radioboxfill',
+          text: '极简模式'
+        }
+      ]
     }
   },
 
   methods: {
-    // #ifndef MP-BAIDU
-    createCircle() {
-      this.$u.route('/pages/components/create-circle-map/create-circle-map')
-    },
-    // #endif
-    // #ifdef MP-BAIDU
-    createCircle() {
-      this.$u.route('/pages/components/create-circle/create-circle')
-    },
-    // #endif
-
     toSearch() {
       this.$u.route('/pages/components/search/search')
+    },
+
+    showModal(e) {
+      this.modalName = e.currentTarget.dataset.target
+    },
+
+    hideModal(e) {
+      this.modalName = null
+    },
+
+    itemClick(text) {
+      this.hideModal()
+      switch (text) {
+        case '地图模式':
+          this.$u.route('/pages/components/create-circle-map/create-circle-map')
+          break
+        case '极简模式':
+          this.$u.route('/pages/components/create-circle/create-circle')
+          break
+      }
     }
   }
 }
@@ -75,5 +121,17 @@ export default {
     transform: translateY(-50%);
     color: rgb(96, 98, 102);
   }
+}
+
+.cutom-modal {
+  width: 300rpx;
+  position: absolute;
+  top: 70rpx;
+  right: 60rpx;
+}
+.custom-icon {
+  position: absolute;
+  top: 30rpx;
+  right: 78rpx;
 }
 </style>
